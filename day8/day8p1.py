3c92321494476c0daa4b4110dc03eefc6a9cf7e5
Chad Hansen <chansen.306@gmail.com>
2015-12-08 00:57:24 -0600

    Removed a debug statement from day8 problem 1

--- day8/day8p1.py
+++ day8/day8p1.py
@@ -6,6 +6,5 @@ for line in sys.stdin:
 	line = line.rstrip('\n')
 	evaluatedLine = eval(line)
 	accum += len(line) - len(evaluatedLine)
-	print( str(len(line) - 1) + ":" + str(len(evaluatedLine)) )
 
 print(accum)
